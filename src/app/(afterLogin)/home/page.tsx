554bc45fa8c5564e32fec4f58f5c1919b78f0869
Sachinandan <sachinandanp5@gmail.com>
2025-03-06 20:05:16 +0530

    sidenav - some fix needed later

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,29 @@
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+// import {
+//   Breadcrumb,
+//   BreadcrumbItem,
+//   BreadcrumbLink,
+//   BreadcrumbList,
+//   BreadcrumbPage,
+//   BreadcrumbSeparator,
+// } from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
 
 export default function page() {
     return (
-        <div>
+        <div className="border-4 w-full">
+            <SidebarTrigger/>
 
-                <div>
-                    Home 
-                </div>
+            <div>
+
+                Home
+            </div>
         </div>
     )
 }
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,7 @@
 import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 import {
+    
+    SidebarFooter,
     Sidebar,
     SidebarContent,
     SidebarGroup,
@@ -17,50 +19,55 @@ const items = [
     {
         title: "Home",
         url: "/home",
-          icon: Home,
+        icon: Home,
     },
     {
         title: "Inbox",
         url: "#",
-          icon: Inbox,
+        icon: Inbox,
     },
     {
         title: "Search",
         url: "#",
-          icon: Search,
+        icon: Search,
     },
     {
         title: "Post",
         url: "#",
-          icon: Calendar,
+        icon: Calendar,
     },
     {
         title: "Profile",
-        url: "#",
-          icon: Calendar,
+        url: "/profile",
+        icon: Calendar,
     },
     {
         title: "Settings",
         url: "#",
-          icon: Settings,
+        icon: Settings,
     },
 ]
 
 
-export function AppSidebar() {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
-        <Sidebar>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <Sidebar variant="sidebar" collapsible="icon" {...props}>
+            {/* <SidebarHeader>
+                Header
+            </SidebarHeader> */}
+
+            <SidebarContent className="flex justify-center">
+                <SidebarGroup className="text-xl" >
+                    {/* <SidebarGroupLabel className=" pl-18">Application</SidebarGroupLabel> */}
                     <SidebarGroupContent>
-                        <SidebarMenu>
+                        {/* pl-18 grid justify-items-center*/}
+                        <SidebarMenu className="text-xl pl-6 ">
                             {items.map((item) => (
                                 <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
+                                    <SidebarMenuButton className="" size={"lg"} asChild>
                                         <a href={item.url}>
                                             <item.icon />
-                                            <span>{item.title}</span>
+                                            <span className="px-4" >{item.title}</span>
                                         </a>
                                     </SidebarMenuButton>
                                 </SidebarMenuItem>
@@ -70,6 +77,9 @@ export function AppSidebar() {
                 </SidebarGroup>
 
             </SidebarContent>
+                <SidebarFooter>
+                <div className="text-center">User: Sachin</div>
+                </SidebarFooter>
         </Sidebar>
     )
 }
