cf244a30f19908270e59f277f2fc4fe521c1d319
SGSTAR01 <startroidx@gmaill.com>
2025-04-06 21:29:43 +0530

    Add remote image patterns to next.config and update ProfilePage to use Next.js Image component

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,13 @@
 import { createAuthClient } from "better-auth/react"
-import { usernameClient,passkeyClient } from "better-auth/client/plugins"
+import { usernameClient,passkeyClient, inferAdditionalFields } from "better-auth/client/plugins"
+import type { auth } from "./auth"
+
 export const authClient = createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL, // the base url of your auth server
     plugins: [
         usernameClient(),
-        passkeyClient()
+        passkeyClient(),
+        inferAdditionalFields<typeof auth>()
     ]
 })
 
